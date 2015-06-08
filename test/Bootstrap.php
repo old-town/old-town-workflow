adfdb7b5a04d3adbeeea845dfba911a8f231b41d
Ìàëîôåéêèí Àíäðåé <malofeykin@platformasoft.ru>
2015-06-08 09:49:50 +0300

    sm-8. Создание теста

--- test/Bootstrap.php
+++ test/Bootstrap.php
@@ -5,7 +5,7 @@
  */
 namespace OldTown\Workflow\Test;
 
-
+use Zend\Loader\AutoloaderFactory;
 use RuntimeException;
 
 error_reporting(E_ALL | E_STRICT);
@@ -41,7 +41,36 @@ class Bootstrap
             return;
         }
 
-        throw new RuntimeException('Unable to load composer autoloader');
+
+        $zf2Path = getenv('ZF2_PATH') ?: (defined('ZF2_PATH') ? ZF2_PATH : (is_dir($vendorPath . '/ZF2/library') ? $vendorPath . '/ZF2/library' : false));
+
+        if (!$zf2Path) {
+            throw new RuntimeException('Unable to load ZF2. Run `php composer.phar install` or define a ZF2_PATH environment variable.');
+        }
+
+        if (isset($loader)) {
+            $loader->add('Zend', $zf2Path . '/Zend');
+        } else {
+            include $zf2Path . '/Zend/Loader/AutoloaderFactory.php';
+            AutoloaderFactory::factory(array(
+                'Zend\Loader\StandardAutoloader' => array(
+                    'autoregister_zf' => true,
+                    'namespaces' => array(
+                        'OldTown\Workflow' => __DIR__ . '/../src/',
+                        __NAMESPACE__ => __DIR__
+                    )
+                )
+            ));
+            AutoloaderFactory::factory(array(
+                'OldTown\Workflow\Test' => array(
+                    'autoregister_zf' => true,
+                    'namespaces' => array(
+                        'OldTown\Workflow' => __DIR__ . '/../test/',
+                        __NAMESPACE__ => __DIR__
+                    )
+                )
+            ));
+        }
     }
 
     /**
@@ -55,7 +84,9 @@ class Bootstrap
         $previousDir = '.';
         while (!is_dir($dir . '/' . $path)) {
             $dir = dirname($dir);
-            if ($previousDir === $dir) return false;
+            if ($previousDir === $dir) {
+                return false;
+            }
             $previousDir = $dir;
         }
         return $dir . '/' . $path;
