f9be569c03114b192a57602cd69a5ab5f3a8171f
Malofeykin Andrey <malofeykin@platformasoft.ru>
2015-06-10 09:46:51 +0300

    fix coding style

---
+++ test/Loader/ArgumentsTraitTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @link    https://github.com/old-town/old-town-workflow
+ * @author  Malofeykin Andrey  <and-rey2@yandex.ru>
+ */
+namespace OldTown\Workflow\Test\Loader;
+
+/**
+ * Class FunctionDescriptorTest
+ *
+ * @package OldTown\Workflow\Test\Loader
+ */
+trait ArgumentsTraitTest
+{
+}
