c50c28c28279eff9e3cbb06d35275c9665fb7806
Malofeykin Andrey <and-rey2@yandex.ru>
2015-06-20 20:48:56 +0300

    sm-7 Портировать ConditionsDescriptor

---
+++ test/Loader/ValidationHelperTest.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * @link    https://github.com/old-town/old-town-workflow
+ * @author  Malofeykin Andrey  <and-rey2@yandex.ru>
+ */
+namespace OldTown\Workflow\Test\Loader;
+
+use OldTown\Workflow\Exception\InvalidWorkflowDescriptorException;
+use PHPUnit_Framework_TestCase as TestCase;
+use OldTown\Workflow\Loader\ValidationHelper;
+use OldTown\Workflow\Loader\ValidateDescriptorInterface;
+
+/**
+ * Class ValidatorDescriptorTest
+ * @package OldTown\Workflow\Test\Loader
+ */
+class ValidationHelperTest extends TestCase
+{
+
+    /**
+     * Тест когда один из элементов коллекции не прошел валидацию
+     *
+     * @expectedException \OldTown\Workflow\Exception\InvalidWorkflowDescriptorException
+     *
+     */
+    public function testNotValidCollection()
+    {
+        $descriptor = $this->getMockForAbstractClass(ValidateDescriptorInterface::class);
+        $exception = new InvalidWorkflowDescriptorException();
+        $descriptor->expects($this->once())->method('validate')->will($this->throwException($exception));
+        $c = [
+            $descriptor
+        ];
+
+        ValidationHelper::validate($c);
+
+    }
+
+    /**
+     * Тест когда коллекция валидна
+     *
+     */
+    public function testValidatorCollection()
+    {
+        $descriptor = $this->getMockForAbstractClass(ValidateDescriptorInterface::class);
+        $c = [
+            $descriptor
+        ];
+
+        ValidationHelper::validate($c);
+    }
+
+
+    /**
+     * Тест пустой коллекции
+     *
+     */
+    public function testEmptyCollection()
+    {
+        $c = [];
+        ValidationHelper::validate($c);
+    }
+
+    /**
+     * Тест когда один из элементов коллекции не прошел валидацию
+     *
+     * @expectedException \OldTown\Workflow\Exception\InvalidWorkflowDescriptorException
+     *
+     */
+    public function testNotValidTraversableCollection()
+    {
+        $descriptor = $this->getMockForAbstractClass(ValidateDescriptorInterface::class);
+        $exception = new InvalidWorkflowDescriptorException();
+        $descriptor->expects($this->once())->method('validate')->will($this->throwException($exception));
+        $c = new \ArrayObject([
+            $descriptor
+        ]);
+
+        ValidationHelper::validate($c);
+
+    }
+}
