c5bc9bf657177267cddd8d91f554685ad7bf8eca
Malofeykin Andrey <and-rey2@yandex.ru>
2015-06-30 23:58:16 +0300

    sm-50 Внедрение Behat

---
+++ test/Behat/features/bootstrap/FeatureContext.php
@@ -0,0 +1,50 @@
+<?php
+
+
+use Behat\Behat\Context\ClosuredContextInterface,
+    Behat\Behat\Context\TranslatedContextInterface,
+    Behat\Behat\Context\BehatContext,
+    Behat\Behat\Exception\PendingException;
+use Behat\Gherkin\Node\PyStringNode,
+    Behat\Gherkin\Node\TableNode;
+
+
+//\test\Behat\features - place your *.feature files here
+//\test\Behat\features\bootstrap - place bootstrap scripts and static files here
+//\test\Behat\features\bootstrap\FeatureContext.php - place your feature related code here
+
+//
+// Require 3rd-party libraries here:
+//
+//   require_once 'PHPUnit/Autoload.php';
+//   require_once 'PHPUnit/Framework/Assert/Functions.php';
+//
+
+/**
+ * Features context.
+ */
+class FeatureContext extends BehatContext
+{
+    /**
+     * Initializes context.
+     * Every scenario gets its own context object.
+     *
+     * @param array $parameters context parameters (set them up through behat.yml)
+     */
+    public function __construct(array $parameters)
+    {
+        // Initialize your context here
+    }
+
+//
+// Place your definition and hook methods here:
+//
+//    /**
+//     * @Given /^I have done something with "([^"]*)"$/
+//     */
+//    public function iHaveDoneSomethingWith($argument)
+//    {
+//        doSomethingWith($argument);
+//    }
+//
+}
